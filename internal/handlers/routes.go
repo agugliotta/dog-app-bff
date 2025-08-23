cc06667ec911c5ede60af9201f18837c0f4d2b9e
Agustin Gugliotta <agustin.mdq.89@gmail.com>
2025-08-23 18:26:24 -0300

    Integrate prometheus
    - Create a /metrics endpoint and expose data for prometheus

--- internal/handlers/routes.go
+++ internal/handlers/routes.go
@@ -1,14 +1,40 @@
 package handlers
 
 import (
+	"strings"
+
 	"github.com/agugliotta/dog-app-bff/internal/store"
 	"github.com/gin-gonic/gin"
+	ginprometheus "github.com/zsais/go-gin-prometheus"
 	"go.uber.org/zap"
 )
 
 func RegisterRoutes(router *gin.Engine, logger *zap.Logger, bs store.BreedStore, ps store.PetStore) {
 	breedHandler := NewBreedHandler(logger, bs)
 	petHandler := NewPetHandler(logger, ps, bs)
+
+	prometheus := ginprometheus.NewWithConfig(ginprometheus.Config{
+		Subsystem: "gin",
+	})
+	prometheus.ReqCntURLLabelMappingFn = func(c *gin.Context) string {
+		url := c.Request.URL.Path
+		for _, p := range c.Params {
+			if p.Key == "id" {
+				url = strings.Replace(url, p.Value, ":id", 1)
+				break
+			} else if p.Key == "slug" {
+				url = strings.Replace(url, p.Value, ":slug", 1)
+				break
+			}
+		}
+		return url
+	}
+	prometheus.Use(router)
+
+	router.GET("/health", func(c *gin.Context) {
+		c.JSON(200, gin.H{"status": "ok"})
+	})
+
 	groupV2 := router.Group("/api/v1")
 
 	groupV2.GET("/breeds", breedHandler.GetBreedsHandler)
